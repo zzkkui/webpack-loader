aef6199cd52ffa00482b49f05a4f9e540efe1fdc
pengkui <kui_peng@kingdee.com>
2022-08-30 19:50:50 +0800

    init

--- src/loader.ts
+++
@@ -1,7 +0,0 @@
-export default function loader(source) {
-  const options = this.getOptions();
-
-  source = source.replace(/\[name\]/g, options.name)
-
-  return `export default ${JSON.stringify(source)}`
-}
